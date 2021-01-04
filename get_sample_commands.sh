2a57c150fd43e6adde2f7d2fb55df066417c178c
sgnoohc <sgnoohc@gmail.com>
2021-01-03 23:47:55 -0800

    update

--- get_sample_commands.sh
+++ get_sample_commands.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
 # To copy all the root files to your laptop, do
-export LPCUSERNAME=pchang # Change the username to yours
+if [ -z $1 ]; then
+    echo "==================================================="
+    echo " Script to download Fireworks exercise root files."
+    echo "==================================================="
+    echo "Usage:"
+    echo "   $0 LPCUSERNAME"
+    echo ""
+fi
+export LPCUSERNAME=${1}
 kinit ${LPCUSERNAME}@FNAL.GOV
 mkdir rootfiles
 scp ${LPCUSERNAME}@cmslpc-sl7.fnal.gov:"/eos/uscms/store/user/cmsdas/2020/short_exercises/Visualization/dy.root" rootfiles/
