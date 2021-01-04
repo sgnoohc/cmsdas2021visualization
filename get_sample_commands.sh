7a2d536d1dd7fbea05e27b4f7e744ba2c7c56723
sgnoohc <sgnoohc@gmail.com>
2021-01-03 23:55:32 -0800

    update

--- get_sample_commands.sh
+++ get_sample_commands.sh
@@ -8,8 +8,10 @@ if [ -z $1 ]; then
     echo "Usage:"
     echo "   $0 LPCUSERNAME"
     echo ""
+    exit
 fi
 export LPCUSERNAME=${1}
+echo "Running kinit..."
 kinit ${LPCUSERNAME}@FNAL.GOV
 mkdir rootfiles
 scp ${LPCUSERNAME}@cmslpc-sl7.fnal.gov:"/eos/uscms/store/user/cmsdas/2020/short_exercises/Visualization/dy.root" rootfiles/
