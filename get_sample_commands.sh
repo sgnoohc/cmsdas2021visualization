c0e7048dcb30ca3fdd4385b3dd8d9e1c70c1b6a6
Philip <sgnoohc@gmail.com>
2021-01-05 09:56:02 -0800

    Update get_sample_commands.sh

--- get_sample_commands.sh
+++ get_sample_commands.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# To copy all the root files to your laptop, do
-if [ -z $1 ]; then
-    echo "==================================================="
-    echo " Script to download Fireworks exercise root files."
-    echo "==================================================="
-    echo "Usage:"
-    echo "   $0 LPCUSERNAME"
-    echo ""
-    exit
-fi
-export LPCUSERNAME=${1}
-PRINCIPAL=$(klist | grep Principal | grep FNAL.GOV)
-while [ -z "${PRINCIPAL}" ]; do
-    echo "No kerberos ticket found!"
-    echo "Running kinit..."
-    kinit ${LPCUSERNAME}@FNAL.GOV
-    PRINCIPAL=$(klist | grep Principal | grep FNAL.GOV)
-done
-# PRINCIPAL=$(klist | grep Principal | grep FNAL.GOV)
+## To copy all the root files to your laptop, do
+#if [ -z $1 ]; then
+#    echo "==================================================="
+#    echo " Script to download Fireworks exercise root files."
+#    echo "==================================================="
+#    echo "Usage:"
+#    echo "   $0 LPCUSERNAME"
+#    echo ""
+#    exit
+#fi
+#export LPCUSERNAME=${1}
+#PRINCIPAL=$(klist | grep Principal | grep FNAL.GOV)
+#while [ -z "${PRINCIPAL}" ]; do
+#    echo "No kerberos ticket found!"
+#    echo "Running kinit..."
+#    kinit ${LPCUSERNAME}@FNAL.GOV
+#    PRINCIPAL=$(klist | grep Principal | grep FNAL.GOV)
+#done
+## PRINCIPAL=$(klist | grep Principal | grep FNAL.GOV)
 mkdir -p rootfiles
 scp ${LPCUSERNAME}@cmslpc-sl7.fnal.gov:"/eos/uscms/store/user/cmsdas/2020/short_exercises/Visualization/dy.root" rootfiles/
 scp ${LPCUSERNAME}@cmslpc-sl7.fnal.gov:"/eos/uscms/store/user/cmsdas/2020/short_exercises/Visualization/ggh4l.root" rootfiles/
